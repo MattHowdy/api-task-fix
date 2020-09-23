5c422616576303f5ffe522e21c94b0a2228f5aad
Mate Hodi <matehodi@Mates-MacBook-Pro.local>
2020-09-23 12:32:32 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+// const mongoose = require('mongoose')
+
+
+dotenv.config()
+const PORT = process.env.BACKEND_PORT || 4001
+
+// mongoose.set('useFindAndModify', false)
+// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+app.use(bodyParser.json({}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+const whitelist = [
+    'http://localhost:4001', 'https://task-fix.herokuapp.com/'
+]
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      try {
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          console.log('Bad cors')
+          callback(null, true)
+        }
+      } catch (error) {
+        console.log('error in cors')
+      }
+    },
+    credentials: true,
+  }
+  
+app.use(cors(corsOptions))
+
+  
+app.use('/', routes)
+
+
+app.listen(PORT, () => {
+ console.log(`server started on port ${PORT}`);
+});
